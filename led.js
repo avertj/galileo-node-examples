bdafb347dacb1d0853e23655925af8e22668b8ea
Julien AVERT <avert.julien@gmail.com>
2015-03-09 19:28:31 +0100

    initial commit

---
+++ led.js
@@ -0,0 +1,22 @@
+var Cylon = require('cylon');
+
+Cylon.robot({
+  connections: {
+    galileo: {
+      adaptor: 'intel-iot'
+    }
+  },
+
+  devices: {
+    led: {
+      driver: 'led',
+      pin: 13
+    }
+  },
+
+  work: function(my) {
+    every((1).second(), function() {
+      my.led.toggle();
+    });
+  }
+}).start();
