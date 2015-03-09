2612dd95bf3e2ff233294e913b013136d0388f58
Julien AVERT <avert.julien@gmail.com>
2015-03-09 21:46:22 +0100

    Essai avec raw GPIO 3

--- led2.js
+++ led2.js
@@ -1,7 +1,8 @@
 var m = require('mraa'); //require mraa
 console.log('MRAA Version: ' + m.getVersion()); //write the mraa version to the console
 
-var myLed = new m.Gpio(13); //LED hooked up to digital pin 13 (or built in pin on Galileo Gen1 & Gen2)
+//var myLed = new m.Gpio(13); //LED hooked up to digital pin 13 (or built in pin on Galileo Gen1 & Gen2)
+var myLed = new m.Gpio(3, true, true); //LED hooked up to digital pin 13 (or built in pin on Galileo Gen1 & Gen2)
 myLed.dir(m.DIR_OUT); //set the gpio direction to output
 var ledState = true; //Boolean to hold the state of Led
 
